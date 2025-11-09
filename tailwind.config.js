a4608328d22afced776696929b1996ad4eec0bff
Eliot Villain <eliot.villain2404@orange.fr>
2025-11-09 17:08:32 +0100

    Correction problème d'affichage du rendu tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Angular : tous les fichiers HTML et TS
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
