65e33d4a4bc807aebe8f42e8eb3635dbc54783e3
Eliot Villain <eliot.villain2404@orange.fr>
2025-11-09 15:57:14 +0100

    Mise a jour de packer pour tailwind et configuration de tailwind

---
+++ src/app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Angular : tous les fichiers HTML et TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
