46c91a4a21aa99479171d7dfe4b7a32efaa805b2
Eliot Villain <eliot.villain2404@orange.fr>
2025-11-09 17:45:12 +0100

    Ajout du tri par nom par defaut et possibilité de trier avec tout les autres champs si désirer.

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -12,6 +12,8 @@ import { CommonModule } from '@angular/common';
 export class UserTableComponent {
   users: User[] = [];
   filterText = '';
+  sortField: keyof User = 'name';
+  sortAsc = true;
 
   constructor(private userService: UserService) {
     this.userService.users$.subscribe((users) => (this.users = users));
@@ -21,5 +23,32 @@ export class UserTableComponent {
     return this.users.filter((user) =>
       user.name.toLowerCase().includes(this.filterText.toLowerCase())
     );
+  }
+
+  sortUsers() {
+    this.users.sort((a, b) => {
+      const valueA = a[this.sortField];
+      const valueB = b[this.sortField];
+
+      // pour les nombres
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return this.sortAsc ? valueA - valueB : valueB - valueA;
+      }
+
+      // pour les chaînes
+      return this.sortAsc
+        ? String(valueA).localeCompare(String(valueB))
+        : String(valueB).localeCompare(String(valueA));
+    });
+  }
+
+  toggleSort(field: keyof User) {
+    if (this.sortField === field) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortField = field;
+      this.sortAsc = true;
+    }
+    this.sortUsers();
   }
 }
